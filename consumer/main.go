11b783b897d2f2b2b417950e587fe6d2d7acb72b
mazzlookman <mohammadlukmanaqib@gmail.com>
2024-01-30 12:47:32 +0700

    create producer and consumer

---
+++ consumer/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	connection, err := amqp091.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+
+	defer connection.Close()
+
+	channel, err := connection.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	consumeWithContext, err := channel.ConsumeWithContext(ctx, "email", "email-consumer", true, false, false, false, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for message := range consumeWithContext {
+		fmt.Println("Routing Key: " + message.RoutingKey)
+		fmt.Println("Body: " + string(message.Body))
+	}
+}
